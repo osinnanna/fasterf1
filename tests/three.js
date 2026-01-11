464d897b0a4407975892ee57f6d854615f3710e7
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-01-11 19:47:11 +0100

    Threejs: made car follow a cirular path
    Also updated tilt of the camera

--- tests/three.js
+++ tests/three.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
+const clock = new THREE.Clock();
 // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // changing 75 to 30
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 10000);
@@ -10,14 +11,25 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 2, 7, 2 );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 20;
+camera.position.y = 10;
+camera.rotation.x = -0.5;
+
+const radius = 4;
 
 function animate() {
+    // Want to try updating the car's positon so it moves in a circular path so thats the z and x positions
+
+    const t = clock.getElapsedTime();
+
+    cube.position.x = radius * Math.cos(t);
+    cube.position.z = radius * Math.sin(t);
+
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render( scene, camera );
