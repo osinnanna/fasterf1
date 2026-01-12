40f2fb04da093ac91c77270cd2a62d20de5a7088
Nnanna Osinachukwu <contact2amondz24@gmail.com>
2026-01-12 21:27:54 +0100

    feat: Slope/lerp functionality using limited points data

---
+++ tests/path.ts
@@ -0,0 +1,11 @@
+type Point = {
+    x: number;
+    z: number;
+}
+const path: { x: number; z: number}[] = [
+    { x: 0, z: 0 },
+    { x: 5, z: 5 },
+    { x: 10, z: 0 },
+    { x: 15, z: -5},
+    { x: 20, z: 0 },
+]
--- tests/three.js
+++ tests/three.js
@@ -2,9 +2,9 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const clock = new THREE.Clock();
-// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// changing 75 to 30
-const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -14,13 +14,26 @@ document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 20;
+camera.position.z = 14;
 camera.position.y = 10;
-camera.rotation.x = -0.5;
+camera.rotation.x = -.6;
 
 const radius = 4;
+
+const cube2 = new THREE.Mesh( geometry, material );
+const path = [
+    { x: 0, z: 0 },
+    { x: 5, z: 5 },
+    { x: 10, z: 0 },
+    { x: 15, z: -5},
+    { x: 20, z: 0 }
+];
+
+let duration = 4;
+const pointCount = path.length - 1;
+
+scene.add( cube, cube2 );
 
 function animate() {
     // Want to try updating the car's positon so it moves in a circular path so thats the z and x positions
@@ -32,6 +45,23 @@ function animate() {
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+
+    // cube 2
+
+    const phase = (t % duration) / duration;
+
+    const indexFloat = phase * pointCount;
+    const index0 = Math.floor(indexFloat);
+    const index1 = Math.ceil(indexFloat);
+    const alpha = indexFloat - index0;
+
+    const p0 = path[index0];
+    const p1 = path[index1 % path.length];
+
+    cube2.position.x = p0.x + (p1.x - p0.x) * alpha;
+    cube2.position.z = p0.z + (p1.z - p0.z) * alpha;
+
     renderer.render( scene, camera );
 }
 
